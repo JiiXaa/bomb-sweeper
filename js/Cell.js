1f516d52c2c5012be08a775387b5b7e057dba0a2
JiiXaa <anythingfx@gmail.com>
2022-11-14 21:25:25 +0000

    logic for generating game board and displaying it when play button is clicked

--- js/Cell.js
+++ js/Cell.js
@@ -1,8 +1,9 @@
 import { LayerControl } from './LayerControl.js';
 
 export class Cell extends LayerControl {
-  constructor(x, y) {
+  constructor(x, y, cellElement) {
     super();
+    this.cellElement = cellElement;
     this.x = x;
     this.y = y;
     this.mine = false;
--- js/GameBoard.js
+++ js/GameBoard.js
@@ -2,6 +2,12 @@ import { LayerControl } from './LayerControl.js';
 import { Cell } from './Cell.js';
 
 const GAME_BOARD_ID = 'js-game-screen';
+const TILE_STATE = {
+  HIDDEN: 'hidden',
+  MINE: 'mine',
+  NUMBER: 'number',
+  MARKED: 'marked',
+};
 
 class GameBoard extends LayerControl {
   difficulties = {
@@ -53,12 +59,31 @@ class GameBoard extends LayerControl {
     for (let x = 0; x < this.rowsCount; x++) {
       const row = [];
       for (let y = 0; y < this.colsCount; y++) {
-        row.push(new Cell(x, y));
+        const cellElement = document.createElement('div');
+        cellElement.classList.add('cell');
+        cellElement.dataset.status = TILE_STATE.HIDDEN;
+        cellElement.dataset.x = x;
+        cellElement.dataset.y = y;
+        cellElement.addEventListener('click', this.handleCellClick);
+        row.push(new Cell(x, y, cellElement));
       }
       this.board.push(row);
     }
-    console.log(this.board);
+    // console.log(this.board);
+    this.board.flat().forEach((cell) => {
+      // console.log(cell.cellElement);
+      this.elementById.appendChild(cell.cellElement);
+    });
   }
+
+  handleCellClick = (e) => {
+    console.log(e.target.dataset);
+    if (e.target.classList.contains('cell--bomb')) {
+      e.target.classList.remove('cell--bomb');
+    } else {
+      e.target.classList.add('cell--bomb');
+    }
+  };
 }
 
 export const gameBoard = new GameBoard();
